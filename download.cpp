3e9be119f9725f7aa6d3e6c602c0875dcbd25e66
Mike Bergmann <mike@mdb977.de>
2018-01-18 11:13:54 +0100

    Updated Comment

--- download.cpp
+++ download.cpp
@@ -1,20 +1,20 @@
 /*
-  Copyright 2014 Mike Bergmann, Bones AG
+  Copyright 2014-2018 Mike Bergmann
 
-  This file is part of WoodscanDownloadManager.
+  This file is part of DownloadManager.
 
-  WoodscanDownloadManager is free software: you can redistribute it and/or modify
+  DownloadManager is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 2 of the License, or
   (at your option) any later version.
 
-  WoodscanDownloadManager is distributed in the hope that it will be useful,
+  DownloadManager is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
 
   You should have received a copy of the GNU General Public License
-  along with WoodscanDownloadManager.  If not, see <http://www.gnu.org/licenses/>.
+  along with DownloadManager.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include "download.h"
